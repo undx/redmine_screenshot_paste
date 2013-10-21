c1c9c50c95605df51d4e296dc3ca05232c083ada
Emmanuel GALLOIS (undx) <emmanuel.gallois@gmail.com>
2013-10-21 19:48:51 +0200

    Added assets_helper.rb from @fishermans contrib

---
+++ lib/assets_helper.rb
@@ -0,0 +1,8 @@
+module AssetsHelper
+  PLUGIN_NAME = File.expand_path('../../*', __FILE__).match(/.*\/(.*)\/\*$/)[1].to_sym
+
+  def self.plugin_asset_link(asset_name,options={})
+    plugin_name=(options[:plugin] ? options[:plugin] : PLUGIN_NAME)
+    File.join(Redmine::Utils.relative_url_root,'plugin_assets',plugin_name.to_s,asset_name)
+  end
+end
